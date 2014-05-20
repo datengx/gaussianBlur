5c52cf5c2b636a7e6a15cf309260999fb1c613b6
Dan <da_teng0702@hotmail.com>
2014-05-20 13:11:42 -0500

    add complex_mult function, add testing in main

--- jni/main.cpp
+++ jni/main.cpp
@@ -3,6 +3,25 @@
 
 #include "include/helloworld.h"
 
+void
+complex_mult(const float *a, const float *b, float *r) {
+
+	double d0, d1, d2, d3;
+
+	asm volatile (
+		"vld1.32	{d0}, [%1]			\n\t"
+		"vld1.32	{d1}, [%2]			\n\t"
+		"vmul.f32	d2, d0, d1			\n\t"
+		"vrev64.32	d3, d0				\n\t"
+		"vmul.f32	d3, d3, d1			\n\t"
+		"vneg.f32	s5, s5				\n\t"
+		"vtrn.32	d2, d3				\n\t"
+		"vadd.f32	d2, d2, d3			\n\t"
+		"vst1.64	{d2}, [%0]			\n\t"
+		:: "r"(r), "r"(a), "r"(b)
+		: "d0", "d1", "d2", "d3", "memory"
+		);
+}
 
 //Time difference function
 timespec diff(timespec start, timespec end)
@@ -27,6 +46,22 @@ timespec diff(timespec start, timespec end)
 
 int main ( int argc, char *argv[] )
 {
+	volatile float a[2], b[2], r[2];
+	int i;
+
+	a[0] = 1;
+	a[1] = 2;
+	b[0] = 3;
+	b[1] = 4;
+
+	for (i=0; i<1e8; i++) {
+#ifdef NEON
+		complex_mult(a, b, r);
+#else
+		r[0] = (a[0]*b[0] - a[1]*b[1]);
+		r[1] = (a[1]*b[0] - a[0]*b[1]);
+	}
+	std::cout << "Result = (" << r[0] << ", " << r[1] << ")" << std::endl;
 
     timespec time1, time2;
 
